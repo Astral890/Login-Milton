811f18dc9b158a90f85d4f151db285ff3ffd6781
Astral890 <enac7125@outlook.com>
2025-02-03 22:41:35 -0600

    final

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const usr=[{email:"roberto@gmail.com",pass:"1357"}]
 
 function checkUsr(event) {
     event.preventDefault();
-    console.log("Formulario detenido. Procesar autenticación aquí.");
     console.log(event.target.emailIn.value);
     LogUsr=event.target.emailIn.value;
     pass=event.target.passIn.value;
