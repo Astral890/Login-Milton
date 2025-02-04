677fbb1540c34f23a7df9b4f7592900a491dd4c6
Astral890 <enac7125@outlook.com>
2025-02-03 22:47:28 -0600

    Final final

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const usr=[{email:"roberto@gmail.com",pass:"1357"}]
 function checkUsr(event) {
     event.preventDefault();
     console.log(event.target.emailIn.value);
-    LogUsr=event.target.emailIn.value;
+    LogUsr=event.target.emailIn.value.toLowerCase();
     pass=event.target.passIn.value;
     tmpPass=usr[0].pass;
     tmpUsr=usr[0].email;
